77292faeeacfc9f1b4daf23397f7842456ae1802
Tolase Durojaye <tolasesaheed@yahoo.co.uk>
2018-10-25 22:48:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+<<<<<<< HEAD
 var number = 10
 makeInt(n)
 var n = makeInt(n)
+=======
+var num = 10
+    n = num 
+    
+    function add2() {
+      return 2 
+    }
+>>>>>>> 1b36fe0a703946f2acd4400026f3420e6bcefbf7
